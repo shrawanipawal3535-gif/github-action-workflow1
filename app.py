cc0e207ec22e9dd92a603b8b8e9fe5b19cd14a37
shrawani pawal <shrawanipawal3535@gmail.com>
2026-03-17 18:27:07 +0530

    fixed newline issue

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -10,3 +11,6 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
+
+
+# 👇 IMPORTANT: one empty line here (press Enter)
