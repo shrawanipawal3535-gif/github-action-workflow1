f2a73a4a9d346b57e69573ab813bca1cd9d41e6b
shrawani pawal <shrawanipawal3535@gmail.com>
2026-03-17 18:37:28 +0530

    added flask8

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -7,9 +8,8 @@ def hello_world():
     return render_template('index.html')
 
 
+# 👇 ADD ONE MORE EMPTY LINE HERE
+
 @app.route('/health')
 def health():
     return 'Server is up and running'
-
-
-# 👇 IMPORTANT: one empty line here (press Enter)
